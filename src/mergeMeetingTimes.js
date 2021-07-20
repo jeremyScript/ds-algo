311c3418d69aa9824ed58a6d94eb8837618c7053
Jeremy Lee <ultrdy@gmail.com>
2021-07-20 17:58:47 -0500

    Add solution to mergeMeetingTimes

--- src/mergeMeetingTimes.js
+++ src/mergeMeetingTimes.js
@@ -25,6 +25,7 @@ the numbers representing our time ranges.
 */
 
 // Solution
+// O(nlog n) time and O(n) space
 
 /**
  * @name mergeMeetingTimes
@@ -32,4 +33,30 @@ the numbers representing our time ranges.
  * @returns {array} An array of merged objects.
  */
 
-export const mergeMeetingTimes = (meetings) => {};
+export const mergeMeetingTimes = (meetings) => {
+  // Create a deep copy of the meetings array
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+  // Sort by startTime
+  const sortedMeetings = meetingsCopy.sort((a, b) => a.startTime - b.startTime);
+  // Create and initialize mergedMeetings
+  const mergedMeetings = [sortedMeetings[0]];
+
+  // Loop through sortedMeetings and merge meetings that overlap
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+    const currentMeeting = sortedMeetings[i];
+
+    // If the meetings overlap, merge them using the later endTime
+    if (lastMergedMeeting.endTime >= currentMeeting.startTime) {
+      lastMergedMeeting.endTime = Math.max(
+        lastMergedMeeting.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      // Add the current meeting otherwise
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
+};

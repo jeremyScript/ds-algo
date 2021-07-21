e8118a453a71f7dc29dc25cde67457dce199dcd2
Jeremy Lee <ultrdy@gmail.com>
2021-07-21 14:39:51 -0500

    Create reverseStringInPlace.js

---
+++ src/reverseStringInPlace.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes an array of characters and reverses the letters in
+place.
+
+Example:
+  Input: ['o', 'l', 'l', 'e', 'h']
+  Output: ['h', 'e', 'l', 'l', 'o']
+*/
+
+// Solution
+
+/**
+ * @name reverseStringInPlace
+ * @param {array} arr - An array of characters or numbers.
+ */
+
+export const reverseStringInPlace = (arr) => {};

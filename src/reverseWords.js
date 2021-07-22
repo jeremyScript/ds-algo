7b94175199365f3c58da48b252a55c8bb8f2b24d
Jeremy Lee <ultrdy@gmail.com>
2021-07-22 14:59:50 -0500

    Create reverseWords.js

---
+++ src/reverseWords.js
@@ -0,0 +1,26 @@
+/*
+Write a function reverseWords() that takes a message as an array of characters
+and reverses the order of the words in place. 
+
+const message = [
+  'c', 'a', 'k', 'e', ' ',
+  'p', 'o', 'u', 'n', 'd', ' ',
+  's', 't', 'e', 'a', 'l'
+];
+
+reverseWords(message);
+
+console.log(message.join(''));  // Prints: 'steal pound cake'
+
+When writing your function, assume the message contains only letters and spaces,
+and all words are separated by one space.
+*/
+
+// Solution
+
+/**
+ * @name reverseWords
+ * @param {*} array - An array of letters and spaces.
+ */
+
+export const reverseWords = (array) => {};

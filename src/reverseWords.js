d1afcecf3400acdf7140d2b0ad8c6c716f14c391
Jeremy Lee <ultrdy@gmail.com>
2021-07-22 16:10:23 -0500

    Add a solution to reverseWords

--- src/reverseWords.js
+++ src/reverseWords.js
@@ -17,10 +17,40 @@ and all words are separated by one space.
 */
 
 // Solution
+// O(n) time and O(1) space
 
 /**
  * @name reverseWords
  * @param {*} array - An array of letters and spaces.
  */
 
-export const reverseWords = (array) => {};
+export const reverseWords = (array) => {
+  reverseStringInPlace(array, 0, array.length);
+  // Define indices that keep track of the start and end of a word
+  let start = 0;
+  let end;
+
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === " " || i === array.length) {
+      end = i;
+      // Whenever the end of a word is detected, reverse the chars within it
+      reverseStringInPlace(array, start, end);
+      start = i + 1;
+    }
+  }
+
+  // A helper function that reverses chars on the opposite ends
+  function reverseStringInPlace(array, start, end) {
+    let leftIndex = start;
+    let rightIndex = end - 1;
+
+    while (leftIndex < rightIndex) {
+      [array[rightIndex], array[leftIndex]] = [
+        array[leftIndex],
+        array[rightIndex],
+      ];
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+};

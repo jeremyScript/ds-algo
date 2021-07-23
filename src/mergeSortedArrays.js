1362a3c690825c87f3c5c9c2323e70c9c1300bac
Jeremy Lee <ultrdy@gmail.com>
2021-07-23 16:07:33 -0500

    Create mergeSortedArrays.js

---
+++ src/mergeSortedArrays.js
@@ -0,0 +1,22 @@
+/*
+Given two arrays of numbers sorted in ascending order,
+write a function to merge the arrays into one sorted array.
+
+Example:
+const array1 = [3, 4, 6, 10, 11, 15];
+const array2 = [1, 5, 8, 12, 14, 19];
+
+mergeSortedArrays(array1, array2);
+// => [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+*/
+
+// Solution
+
+/**
+ * @name mergeSortedArrays
+ * @param {array} arr1 - A sorted array.
+ * @param {array} arr2 - A sorted array.
+ * @returns {array} A merged and sorted array.
+ */
+
+export const mergeSortedArrays = (arr1, arr2) => {};

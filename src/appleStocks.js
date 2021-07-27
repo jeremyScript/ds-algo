00005f80cbbc2f48cfe13626853e681e33490e24
Jeremy Lee <ultrdy@gmail.com>
2021-07-27 11:03:54 -0500

    Create appleStocks.js

---
+++ src/appleStocks.js
@@ -0,0 +1,24 @@
+/*
+Write an efficient function that takes stockPricesYesterday and returns the best
+profit I could have made from one purchase and one sale of one share of Apple
+stock yesterday.
+
+For example:
+
+  const stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+  getMaxProfit(stockPricesYesterday);
+  // Returns 6 (buying for $5 and selling for $11)
+
+  No "shorting" — you need to buy before you can sell. Also, you can't buy and
+  sell in the same time step—at least 1 minute has to pass.
+*/
+
+// Solution
+
+/**
+ * @name getMaxProfit
+ * @param {array} stockPrices - An array of stock prices.
+ * @returns {number} A number representing the maximum profit.
+ */
+
+export const getMaxProfit = (stockPrices) => {};

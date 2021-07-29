2b5ba7491e9822bf24491224fcaeeaf08af865aa
Jeremy Lee <ultrdy@gmail.com>
2021-07-29 10:41:28 -0500

    Create highestProductOf3.js

---
+++ src/highestProductOf3.js
@@ -0,0 +1,16 @@
+/*
+Given an array of integers, find the highest product you can get from three of
+the integers.
+
+The input arrayOfInts will always have at least three integers.
+*/
+
+// Solution
+
+/**
+ * @name getHighestProductOf3
+ * @param {array} arrayOfInts - An array of integers.
+ * @return {number} The highest possible product of 3 integers.
+ */
+
+export const getHighestProductOf3 = (arrayOfInts) => {};

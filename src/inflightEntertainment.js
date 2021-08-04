aa22f1457b1031e92b713a03ef003188d0faed86
Jeremy Lee <ultrdy@gmail.com>
2021-08-04 12:03:49 -0500

    Create inflightEntertainment.js

---
+++ src/inflightEntertainment.js
@@ -0,0 +1,21 @@
+/*
+Write a function that takes an integer flightLength (in minutes) and an array of
+integers movieLengths (in minutes) and returns a boolean indicating whether
+there are two numbers in movieLengths whose sum equals flightLength.
+
+When building your function:
+- Assume your users will watch exactly two movies
+- Don't make your users watch the same movie twice
+- Optimize for runtime over memory
+*/
+
+// Solution
+
+/**
+ * @name canTwoMoviesFillFlight
+ * @param {array} movieLengths - An array of movie runtimes in minutes.
+ * @param {number} flightLength - An integer for flight duration in minutes.
+ * @returns {boolean} A boolean indicating whether two such films exist.
+ */
+
+export const canTwoMoviesFillFlight = (movieLengths, flightLength) => {};

dadf4c1d17d0743a8d8c8acd1c9fbdc78feab102
Jeremy Lee <ultrdy@gmail.com>
2021-08-05 17:02:30 -0500

    Create permutationPalindrome.js

---
+++ src/permutationPalindrome.js
@@ -0,0 +1,25 @@
+/*
+Write an efficient function that checks whether any permutation of an input
+string is a palindrome.
+
+You can assume the input string only contains lowercase
+letters.
+
+Examples:
+"civic" should return true
+"ivicc" should return true
+"civil" should return false
+"livci" should return false
+*/
+
+// Solution
+
+/**
+ * @name hasPalindromePermutation
+ * @param {string} str - A string of lowercase letters.
+ * @returns {boolean} A boolean indicating whether there's a palindrome.
+ */
+
+// A string has a permutation that's a palindrome if it has no more than one unique letter
+// that occurs an odd number of times
+export const hasPalindromePermutation = (str) => {};

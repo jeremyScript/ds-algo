56be779405eafc970d7b14da47eb57d002f45f91
Jeremy Lee <ultrdy@gmail.com>
2021-08-06 16:51:25 -0500

    Create wordCloudData.js

---
+++ src/wordCloudData.js
@@ -0,0 +1,16 @@
+/*
+You want to build a word cloud, an infographic where the size of a word
+corresponds to how often it appears in the body of text.
+
+Write code that takes a long string and builds its word cloud data in a
+Map, where the keys are words and the values are the number of times the words
+occurred.
+
+Assume the input will only contain words and standard capitalization and
+punctuation. Your code should reasonably handle the same word with different
+capitalization.
+*/
+
+// Solution
+
+const buildWordCloudData = (str) => {};

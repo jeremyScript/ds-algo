83a24b6ac3887a17c5b4276b01d04e33c8d2170b
Jeremy Lee <ultrdy@gmail.com>
2021-08-06 16:54:51 -0500

    Create wordCloudData.test.js

---
+++ tests/wordCloudData.test.js
@@ -0,0 +1,40 @@
+import { buildWordCloudData } from "../src/wordCloudData";
+
+test("given a simple-structured string, it builds a map correctly", () => {
+  const str = "Rather than love, than money, than fame, give me truth.";
+  const actual = buildWordCloudData(str);
+  const expected = new Map([
+    ["Rather", 1],
+    ["than", 3],
+    ["love", 1],
+    ["money", 1],
+    ["fame", 1],
+    ["give", 1],
+    ["me", 1],
+    ["truth", 1],
+  ]);
+  expect(actual).toEqual(expected);
+});
+
+test("Capitalized words are kept as such only if they're consistent", () => {
+  const str =
+    "All I know is that when I needed McDonald's, McDonald's was there for me all the time.";
+  const actual = buildWordCloudData(str);
+  const expected = new Map([
+    ["all", 2],
+    ["I", 2],
+    ["know", 1],
+    ["is", 1],
+    ["that", 1],
+    ["when", 1],
+    ["needed", 1],
+    ["McDonald's", 2],
+    ["was", 1],
+    ["there", 1],
+    ["for", 1],
+    ["me", 1],
+    ["the", 1],
+    ["time", 1],
+  ]);
+  expect(actual).toEqual(expected);
+});

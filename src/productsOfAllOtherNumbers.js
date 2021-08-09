e26fc456afc28e2f99f61f16433c6ab2224b49ad
Jeremy Lee <ultrdy@gmail.com>
2021-08-09 11:00:44 -0500

    Create productsOfAllOtherNumbers.js

---
+++ src/productsOfAllOtherNumbers.js
@@ -0,0 +1,26 @@
+/*
+Write an efficient function that takes an array of integers and returns an array
+of the products calculated by multiplying every integer in the given array
+except the integer at that index for every index.
+
+For example:
+
+  Given,
+  [1, 7, 3, 4],
+  the function would return
+  [84, 12, 28, 21]
+  by calculating
+  [7 * 3 * 4, 1 * 3 * 4, ..., 1 * 7 * 3]
+
+Do not use division in the solution.
+*/
+
+// Solution
+
+/**
+ * @name getProductsOfAllOtherNumbers
+ * @param {array} numbers - An array of integers.
+ * @returns {array} An array consisting of products of integers.
+ */
+
+export const getProductsOfAllOtherNumbers = (numbers) => {};

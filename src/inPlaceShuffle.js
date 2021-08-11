1c36519d65a38cc4844560d22edfec5cd81ef66c
Jeremy Lee <ultrdy@gmail.com>
2021-08-11 14:49:42 -0500

    Create inPlaceShuffle.js

---
+++ src/inPlaceShuffle.js
@@ -0,0 +1,17 @@
+/*
+Write a function for doing an in-place shuffle of an array. The shuffle must be
+"uniform", meaning each item in the original array must have the same
+probability of ending up in each spot in the final array.
+
+Assume that you have a function getRandom(floor, ceiling) for getting a random
+integer that is >= floor and <= ceiling.
+*/
+
+// Solution
+
+/**
+ * @name shuffleArrayInPlace
+ * @param {array} array - An array to be shuffled.
+ */
+
+export const shuffleArrayInPlace = (array) => {};

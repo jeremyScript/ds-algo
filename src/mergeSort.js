cc0026c5e1cf43dcc07a0291e80b8a773d6a5037
Jeremy Lee <ultrdy@gmail.com>
2021-09-07 13:11:10 -0500

    Create mergeSort.js

---
+++ src/mergeSort.js
@@ -0,0 +1,20 @@
+/*
+Given an array of numbers, write a function that will sort the numbers using the
+merge sort method.
+
+Example:
+const arr = [6, 2, 4, 0, -1, 4, 2];
+
+mergeSort(arr);
+// => [-1, 0, 2, 2, 4, 4, 6]
+*/
+
+// Solution
+
+/**
+ * @name mergeSort
+ * @param {array} arr - An unsorted array of numbers.
+ * @returns {array} A sorted array.
+ */
+
+export const mergeSort = (arr) => {};

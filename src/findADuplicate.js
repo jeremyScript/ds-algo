317a02d3a0fa6aee1568cbe865c7ef60d8ff668b
Jeremy Lee <ultrdy@gmail.com>
2021-09-07 11:07:44 -0500

    Create findADuplicate.js

---
+++ src/findADuplicate.js
@@ -0,0 +1,26 @@
+/*
+There's an array of integers, where:
+
+1. The integers are in the range 1..n.
+2. The array has a length of n+1.
+
+It follows that the array has at least one integer which appears more than once.
+But it may have several duplicates, and each duplicate may appear twice or more.
+
+Write a function which finds an integer that appears more than once in an array.
+If there are multiple duplicates, you only need to find one of them.
+
+Do not modify the input.
+
+Optimize for space.
+*/
+
+// Solution
+
+/**
+ * @name findADuplicate
+ * @param {array} numbers - An array of numbers.
+ * @returns {number or null} A number that is repeated. If none, null.
+ */
+
+export const findADuplicate = (numbers) => {};

7890e187f8a37d1010cde35ba145110175ea6c53
Jeremy Lee <ultrdy@gmail.com>
2021-09-06 20:26:30 -0500

    Create findRotationPoint.js

---
+++ src/findRotationPoint.js
@@ -0,0 +1,33 @@
+/*
+Given an array of words that are alphabetically ordered except the start of the
+list of words is not at the very top or at index 0.
+
+For example:
+
+  const words = [
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist',
+  'asymptote',  // <-- rotates here!
+  'babka',
+  'banoffee',
+  'engender',
+  'karpatka',
+  'othellolagkage',
+];
+
+Write a function for finding the index of the "rotation point", which is where
+the start of the list is. Assume the list is huge so we want efficiency here.
+*/
+
+// Solution
+
+/**
+ * @name findRotationPoint
+ * @param {array} words - An array of words in a rotated, alphabetical order.
+ * @reutrns {number} The index at which the list of words rotates.
+ */
+
+export const findRotationPoint = (words) => {};

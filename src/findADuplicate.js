6f41fc2284fa487503f05bffa33987d5b8205fb6
Jeremy Lee <ultrdy@gmail.com>
2021-09-09 17:56:34 -0500

    Add space enhanced solution

--- src/findADuplicate.js
+++ src/findADuplicate.js
@@ -16,6 +16,7 @@ Optimize for space.
 */
 
 // Solution
+// O(nlog n) time and O(1) space
 
 /**
  * @name findADuplicate
@@ -23,7 +24,50 @@ Optimize for space.
  * @returns {number} A number that is repeated.
  */
 
-export const findADuplicate = (numbers) => {
+export function findADuplicate(numbers) {
+  let floor = 1;
+  let ceiling = numbers.length - 1;
+
+  while (floor < ceiling) {
+    const midpoint = Math.floor(floor + (ceiling - floor) / 2);
+    const lowerRangeFloor = floor;
+    const lowerRangeCeiling = midpoint;
+    const upperRangeFloor = midpoint + 1;
+    const upperRangeCeiling = ceiling;
+
+    const distinctPossibleIntegersInLowerRange =
+      lowerRangeCeiling - lowerRangeFloor + 1;
+
+    // Count number of items in lower range
+    let itemsInLowerRange = 0;
+    numbers.forEach((item) => {
+      // Is it in the lower range?
+      if (item >= lowerRangeFloor && item <= lowerRangeCeiling) {
+        itemsInLowerRange += 1;
+      }
+    });
+
+    if (itemsInLowerRange > distinctPossibleIntegersInLowerRange) {
+      // There must be a duplicate in the lower range
+      // so use the same approach iteratively on that range
+      floor = lowerRangeFloor;
+      ceiling = lowerRangeCeiling;
+    } else {
+      // There must be a duplicate in the upper range
+      // so use the same approach iteratively on that range
+      floor = upperRangeFloor;
+      ceiling = upperRangeCeiling;
+    }
+  }
+
+  // Floor and ceiling have converged
+  return floor;
+}
+
+// Alternative Solution
+// O(n) time and O(n) space
+
+export const findADuplicate2 = (numbers) => {
   const setOfNums = new Set();
   for (let num of numbers) {
     if (setOfNums.has(num)) return num;

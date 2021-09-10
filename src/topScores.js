2931dae40b248cc669d5f65a45c7072d4591af98
Jeremy Lee <ultrdy@gmail.com>
2021-09-10 12:23:24 -0500

    Add comments

--- src/topScores.js
+++ src/topScores.js
@@ -18,8 +18,10 @@ Do this in less than O(nlog n) time.
 */
 
 // Solution
+// O(n) time and O(n) space
 
 export const sortScores = (unsortedScores, HIGHEST_POSSIBLE_SCORE) => {
+  // Store scores as indices in a temporary array
   const tempArr = [];
   for (let i = 0; i < unsortedScores.length; i++) {
     const score = unsortedScores[i];
@@ -27,11 +29,11 @@ export const sortScores = (unsortedScores, HIGHEST_POSSIBLE_SCORE) => {
       throw new Error("Score outside the range");
     tempArr[score] = tempArr[score] === undefined ? 1 : tempArr[score] + 1;
   }
-
+  // Switch the indices back to scores in a new array
   const sortedArr = [];
-
   for (let i = 0; i < tempArr.length; i++) {
     let count = tempArr[i] || 0;
+    // If there are multiple entries of same value
     while (count > 0) {
       sortedArr.push(i);
       count--;

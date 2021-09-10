3f24e0cdeb8781d7ea50fe9eaf372c8dd909f6af
Jeremy Lee <ultrdy@gmail.com>
2021-09-10 11:52:56 -0500

    Create topScores.js

---
+++ src/topScores.js
@@ -0,0 +1,22 @@
+/*
+Write a function that takes:
+
+  1) an array of unsorted scores and
+  2) the highest possible score in the game
+
+  and returns a sorted array of scores.
+
+Example:
+
+const unsortedScores = [37, 89, 41, 65, 91, 53];
+const HIGHEST_POSSIBLE_SCORE = 100;
+
+sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE);
+// returns [91, 89, 65, 53, 41, 37]
+
+Do this in less than O(nlog n) time.
+*/
+
+// Solution
+
+export const sortScores = (unsortedScores, HIGHEST_POSSIBLE_SCORE) => {};

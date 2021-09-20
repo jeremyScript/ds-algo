7bd69d8a632d5a100da0f2e0a7c16e1e9af5edbd
Jeremy Lee <ultrdy@gmail.com>
2021-09-20 10:49:55 -0500

    Create queue.js

---
+++ src/queue.js
@@ -0,0 +1,7 @@
+/*
+Implement a queue. Your queue should have enqueue and dequeue methods and
+should be FIFO. Your queue implementation should also have time and space 
+complexities that are characteristic of a queue.
+*/
+
+// Solution

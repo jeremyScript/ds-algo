e9ebffaf813bf216b8e417d3f1fcbbe193e19343
Jeremy Lee <ultrdy@gmail.com>
2021-09-20 12:06:34 -0500

    Add solution to queue

--- src/queue.js
+++ src/queue.js
@@ -5,3 +5,43 @@ complexities that are characteristic of a queue.
 */
 
 // Solution
+
+class Queue {
+  constructor() {
+    this.head = this.createNodeFromValue(null);
+    this.tail = this.head.next;
+  }
+
+  createNodeFromValue(value) {
+    return {
+      value,
+      next: null,
+    };
+  }
+
+  enqueue(value) {
+    const newNode = this.createNodeFromValue(value);
+    if (this.head) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+  }
+
+  dequeue() {
+    if (this.head === null) return null;
+    const dequeuedValue = this.head.value;
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
+    this.head = this.head.next;
+
+    return dequeuedValue;
+  }
+
+  peek() {
+    return this.tail.value;
+  }
+}

7439d520b14d0518f52f90bbd43f422689b43293
Jeremy Lee <ultrdy@gmail.com>
2022-04-07 13:42:08 -0500

    Add solution

--- src/twoStackQueue.js
+++ src/twoStackQueue.js
@@ -10,4 +10,70 @@ Assume you already have a stack implementation.
 
 // Solution
 
-export class TwoStackQueue {}
+export class TwoStackQueue {
+  constructor() {
+    this._inStack = new Stack();
+    this._outStack = new Stack();
+    this.length = 0;
+  }
+
+  enqueue(item) {
+    this._inStack.push(item);
+    return ++this.length;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    this.length--;
+
+    return this._prepOutStack().pop();
+  }
+
+  peek() {
+    return this.length === 0 ? null : this._prepOutStack().peek();
+  }
+
+  _prepOutStack() {
+    if (this._outStack.top === 0) {
+      while (this._inStack.top > 0) {
+        this._outStack.push(this._inStack.pop());
+      }
+    }
+    return this._outStack;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.data = [];
+    this.top = 0;
+  }
+
+  push(element) {
+    this.data[this.top++] = element;
+    return this.top;
+  }
+
+  pop() {
+    if (this.top === 0) return null;
+
+    this.top--;
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.top === 0 ? null : this.data[this.top - 1];
+  }
+
+  isEmpty() {
+    return this.top === 0 ? true : false;
+  }
+
+  clear() {
+    this.data = [];
+    this.top = 0;
+  }
+}

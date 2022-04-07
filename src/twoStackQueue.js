da384e59a54cffb0cc34219c891e083c475baeac
Jeremy Lee <ultrdy@gmail.com>
2022-04-07 11:27:27 -0500

    Create twoStackQueue.js

---
+++ src/twoStackQueue.js
@@ -0,0 +1,13 @@
+/*
+Implement a queue with 2 stacks.
+Your queue should have an enqueue and a dequeue method.
+
+Optimize for the time cost of m calls on your queue.
+These can be any mix of enqueue and dequeue calls.
+
+Assume you already have a stack implementation.
+*/
+
+// Solution
+
+export class TwoStackQueue {}

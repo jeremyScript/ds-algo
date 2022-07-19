d1c05cc9d3971faaa04beb0b265739a250059ac8
Jeremy Lee <ultrdy@gmail.com>
2022-07-19 10:21:31 -0500

    Create balancedBinaryTree

---
+++ src/balancedBinaryTree.js
@@ -0,0 +1,12 @@
+/*
+Write a function to see if a binary tree is "superbalanced".
+
+A tree is "superbalanced" if the difference between the depths of any two leaf
+nodes is no greater than one.
+*/
+
+// Solution
+
+export const isBinaryTreeBalanced = (treeRoot) => {
+  // returns boolean
+};
